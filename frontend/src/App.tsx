626c92d36df9e9f655e3b1f1a3d0ef677fefee3d
한지은 <hjn510450@gmail.com>
2025-12-18 19:15:51 +0900

    feat: 템플릿 제공 및 업로드 기능 구현

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,11 @@
-import { ReservationList } from './features/reservation-list';
+import { Toaster } from 'sonner';
 import { CreateEventPage } from './features/create-event/ui/CreateEventPage';
-import './features/reservation-list/styles.css';
-
-function App() {
-  // TODO: 라우팅 추가 필요 - 임시로 ReservationList만 렌더링
-  return <ReservationList />;
-import { Toaster } from "sonner";
-import { CreateEventPage } from "./features/create-event/ui/CreateEventPage";
-import ReservationPage from "./features/reservation/ui/ReservationPage";
 
 function App() {
   return (
     <>
       <Toaster position="top-center" richColors />
       <CreateEventPage />
-      <ReservationPage />
     </>
   );
 }
