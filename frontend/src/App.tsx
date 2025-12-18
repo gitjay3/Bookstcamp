c52ee1161a7143e890cd6b18f238ab51fdcfdd82
gitjay3 <js20525@naver.com>
2025-12-18 14:22:42 +0900

    fix: 머지 충돌한 코드 순서 정리

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,3 @@
-import { ReservationList } from './features/reservation-list';
-import { CreateEventPage } from './features/create-event/ui/CreateEventPage';
-import './features/reservation-list/styles.css';
-
-function App() {
-  // TODO: 라우팅 추가 필요 - 임시로 ReservationList만 렌더링
-  return <ReservationList />;
 import { Toaster } from "sonner";
 import { CreateEventPage } from "./features/create-event/ui/CreateEventPage";
 import ReservationPage from "./features/reservation/ui/ReservationPage";
