d5b9c08d71ea11af9140dad406cb517f03123897
한지은 <hjn510450@gmail.com>
2026-01-12 16:34:00 +0900

    fix: 이벤트 타입에 id, applicationUint(예약 단위 - 팀/개인) 필드 추가

--- frontend/src/types/event.ts
+++ frontend/src/types/event.ts
@@ -1,11 +1,14 @@
 export type EventCategory = 'WEB' | 'ANDROID' | 'IOS' | 'COMMON';
 export type EventStatus = 'ONGOING' | 'UPCOMING' | 'ENDED';
+export type ApplicationUnit = 'INDIVIDUAL' | 'TEAM';
 
 export interface Event {
+  id: number;
   category: EventCategory;
   status: EventStatus;
   title: string;
   description: string;
   startAt: Date;
   endAt: Date;
+  applicationUnit: ApplicationUnit;
 }
