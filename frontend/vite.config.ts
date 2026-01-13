39040fd5140d5ed65e459370d4670ba86d9960a1
RainWhales <wkengmlwo@gmail.com>
2026-01-14 04:47:40 +0900

    chore: BE 프록시 포트 수정

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -21,7 +21,7 @@ export default defineConfig({
     proxy: {
       // '/api'로 시작하는 요청을 백엔드로 프록시
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'http://localhost:3001',
         changeOrigin: true,
       },
     },
